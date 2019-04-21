615da6da8e7e9ece2c803ccb5ab823ef81111d3b
Alexander <sashka0264@yandex.ru>
2019-04-21 20:04:37 +0300

    Исправил ошибки

--- lesson-2.1 hard/lesson-2.js
+++ lesson-2.1 hard/lesson-2.js
@@ -1,27 +1,26 @@
 'use strict';
 
-// let week = ["Monday", "Tuesday", "Wednesday", "Thursday", 
-// "Friday", "Saturday", "Sunday"];
+let week = ["Monday", "Tuesday", "Wednesday", "Thursday", 
+"Friday", "Saturday", "Sunday"];
 
-// let i,
-//     a;
 
-// for (a=0; a<7;) {
-//     if (a < 5) {
-//         document.write(week[a] + "<br>");
-//     }
-//     if (a == 5) {
-//         document.write("<b>" + week[a] + "</b><br>");
-//         i++;
-//     }
-//     if (a == 6) {
-//         document.write("<b><i>" + week[a] + "</i></b><br>");
-//         i++;
-//     }
-//     a++;
-// }
+    
 
-// let f;
+for (let a = 0; a < 7;) {
+    if (a < 5) {
+        document.write(week[a] + "<br>");
+    }
+    if (a == 5) {
+        document.write("<b>" + week[a] + "</b><br>");
+    
+    }
+    if (a == 6) {
+        document.write("<b><i>" + week[a] + "</i></b><br>");
+    }
+    a++;
+}
+
+
 
 let arr = ['10', '11', '12', '13', '34', '15', '76'];
 
