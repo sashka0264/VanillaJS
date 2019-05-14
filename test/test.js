09079ebeecedc3d8953c45e3774d5deeacade3b5
Alexander <sashka0264@yandex.ru>
2019-05-14 17:12:31 +0300

    тесты

---
+++ test/test.js
@@ -0,0 +1,31 @@
+function sayName(name) {
+    let message = "My name is" + name;
+    return message;
+}
+
+let arr = [5, 3, -1];
+let result = arr.reduce(function(sum, elem) {
+    return sum + elem;
+});
+
+// Сам тест
+
+
+let assert = require('chai').assert;
+
+
+describe("sayName", function() {
+    
+    it("Получаем фразу с новым именем", function() {
+        assert.typeOf(sayName("DSsimon"), "string");
+    });
+});
+
+describe("arr", function() {
+    
+    it("Получаем сумму чисел массива", function() {
+        assert.equal(result, 7);
+    });
+})
+// Сам тест 
+
