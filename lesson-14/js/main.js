6075e5ed89a77ba310efc029e6acece4678abb13
Alexander <sashka0264@yandex.ru>
2019-05-16 21:37:15 +0300

    Сделал основное и доп

--- lesson-14/js/main.js
+++ lesson-14/js/main.js
@@ -36,16 +36,17 @@ $(document).ready(function () {
         $(".modal").animate({"top": "30px", "opacity": "1"}, "slow");
     });
 
-    $("#sheldures").on("click", function() {
-       
+    $('.main_nav a[href="#sheldure"]').on("click", function() {
+ 
         $(".overlay").fadeIn("slow"); 
         $(".modal").css({"top": "-40px", "display": "block", "opacity": "0"});
         $(".modal").animate({"top": "30px", "opacity": "1"}, "slow");
     });
 
     $(".close").on("click", function() {
-        $(".overlay").fadeToggle("slow"); 
-        $(".modal").css({"display": "none"});
+        $(".overlay").fadeToggle("slow");
+        $(".modal").animate({"top": "-30px", "opacity": "0"}, 500);
+        $(".modal").fadeToggle(500); 
     });
     
 
