250caf32c53530b978a4f0c4c1948e193bfeb605
Alexander <sashka0264@yandex.ru>
2019-08-18 21:27:42 +0300

    Добавил файлы

---
+++ Calculator/script.js
@@ -0,0 +1,66 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+    let items = document.querySelectorAll(".calculator-buttons__item"),
+        result = document.querySelector(".calculator__result");
+
+    
+
+    items.forEach((item, i) => {
+        item.addEventListener("click", () => {
+
+            let x = items[i].textContent;
+
+            if (result.textContent !== "0") {
+                if (items[i].classList.contains("calculator-buttons__item-number") == true ||
+                    items[i].classList.contains("calculator-buttons__item-symbol") == true) {
+
+                    result.textContent = result.textContent + x;
+                }
+            } else {
+                result.textContent = x;
+            }
+
+           
+            if (items[i].classList.contains("calculator-buttons__item-symbol") == true) {
+                let arr = (result.textContent).split("");
+
+                if (arr[arr.length-1] == arr[arr.length-2] || arr[arr.length-2] == "*" ||
+                    arr[arr.length-2] == "-" || arr[arr.length-2] == "+" || arr[arr.length-2] == "/") {
+                    let x = arr[arr.length-1];
+                    arr.pop();
+                    arr.pop();
+                    arr.push(x);
+                    result.textContent = arr.join("");
+                    console.log("Повторение"); 
+                }
+            }
+            // Повторное нажатие символов
+            
+
+            if (items[i].textContent == "=") {
+                if (result.textContent !== "=") {
+                result.textContent = eval(result.textContent);
+                } else {
+                    result.textContent = "0";
+                }
+            }
+            // Кнопка "="
+
+            if (items[i].textContent == "→" && result.textContent !== "0") {
+                let arr = (result.textContent).split("");
+                arr.pop();
+                if (arr.length == 0) {
+                    arr.push(0);
+                }
+                result.textContent = arr.join("");
+            }
+            // Кнопка "→"
+
+            if (items[i].textContent == "с") {
+                result.textContent = "0";
+            }
+            // Кнопка "remove"
+            console.log(result.textContent);
+        });
+    });
+});
--- task-1/script.js
+++ task-1/script.js
@@ -1,10 +1,31 @@
-// Вывести все ключи/значения обьекта через цикл for in
-let x = {
+// 1 Вывести все ключи/значения обьекта через цикл for in и добавить их в 
+// новый обьект y
+
+/*let x = {
     x1: 5,
     x2: 323,
     x3: 0,
 };
 
+let y = {};
+
 for (let key in x) {
     console.log(key, x[key]);
 }
+
+for (let key in x) {
+    y[key] = x[key];
+}
+console.log(y);*/
+
+let x = document.querySelector("div");
+
+
+
+x.addEventListener("mouseover", function(e) {
+
+    
+    console.log(this.attributes);
+
+});
+
