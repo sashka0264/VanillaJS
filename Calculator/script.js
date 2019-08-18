a6356e8a9af6f1870638c377d380caa6c23b2cbb
Alexander <sashka0264@yandex.ru>
2019-08-19 01:38:22 +0300

    Внес коррективы

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,9 +1,10 @@
 window.addEventListener("DOMContentLoaded", () => {
 
     let items = document.querySelectorAll(".calculator-buttons__item"),
-        result = document.querySelector(".calculator__result");
+        result = document.querySelector(".calculator__result"),
+        error = document.querySelector(".calculator__error");
 
-    
+
 
     items.forEach((item, i) => {
         item.addEventListener("click", () => {
@@ -20,26 +21,37 @@ window.addEventListener("DOMContentLoaded", () => {
                 result.textContent = x;
             }
 
-           
-            if (items[i].classList.contains("calculator-buttons__item-symbol") == true) {
+
+            if (items[i].classList.contains("calculator-buttons__item-symbol") == true &&
+                items[i].classList.contains("bracket") == false) {
                 let arr = (result.textContent).split("");
 
-                if (arr[arr.length-1] == arr[arr.length-2] || arr[arr.length-2] == "*" ||
-                    arr[arr.length-2] == "-" || arr[arr.length-2] == "+" || arr[arr.length-2] == "/") {
-                    let x = arr[arr.length-1];
+                if (arr[arr.length - 1] == arr[arr.length - 2] || arr[arr.length - 2] == "*" ||
+                    arr[arr.length - 2] == "-" || arr[arr.length - 2] == "+" || arr[arr.length - 2] == "/") {
+                    let x = arr[arr.length - 1];
                     arr.pop();
                     arr.pop();
                     arr.push(x);
                     result.textContent = arr.join("");
-                    console.log("Повторение"); 
+                    console.log("Повторение");
                 }
             }
             // Повторное нажатие символов
-            
+
 
             if (items[i].textContent == "=") {
                 if (result.textContent !== "=") {
-                result.textContent = eval(result.textContent);
+
+                    try {
+                        result.textContent = eval(result.textContent);
+                    } catch (e) {
+                        error.classList.add("calculator__error-active");
+                        // Вот
+                        setTimeout( () => {
+                            error.classList.remove("calculator__error-active");
+                        }, 5000);
+                    } 
+
                 } else {
                     result.textContent = "0";
                 }
