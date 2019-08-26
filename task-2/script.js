28642945e5469cbe466e67c307d4cc62788b465c
Alexander <sashka0264@yandex.ru>
2019-08-27 02:29:06 +0300

    Внес правки

--- task-2/script.js
+++ task-2/script.js
@@ -42,51 +42,41 @@ sortFunc(str1); */
 
 // 3 Дан массив a c целыми элементами. Необходимо "сжать" а , выбросив из него 0
 
-/* let arr = [1, 5, 4, 0, 0, 0, 3, 1, 10, 142];
+/*let arr = [1, 5, 4, 0, 0, 0, 3, 1, 10, 142];
 
 let clear = (arr) => {
 
     for (let i = 0; i < arr.length; i++) {
 
         if (arr[i] == 0) {
-    
             arr.splice(i, 1);
             i--;
-            
-        } 
-        
-        if (i == arr.length - 1) {
+        } else if (i == arr.length - 1) {
             console.log(arr);
         }
-    
     }
-
 };
-clear(arr); */
+clear(arr);*/
 
 // 4 Использование метода .join() и ! после каждого, кроме последнего
 
-/* let x = ['!', 1, 0, 5, -11];
+/*let x = ['!', 1, 0, 5, -11];
 
 for (let i = 0; i < x.length; i++) {
     
     if (i !== x.length - 1) {
-
-        if (x[i] == "!") {
-            console.log("fuck you, user");
-        } else {
+        if (x[i] !== "!") {
             x.splice((i+1), 0, "!");
             i++;
         }
-    
     } else {
-        console.log(x.join());
+        console.log(x.join(""));
     }
-} */
+}*/
 
 // 5 Проверка массива на монотонность
 
-/* let arr3 = [0, 1, 5, 9, 15],
+/*let arr3 = [0, 1, 5, 9, 15],
     arr4 = [0, 1, 1, 5, 9, 9, 15],
     arr2 = [15, 8, 4, 2, 1],
     arr1 = [0, 1, 5, 15, 4],
@@ -103,35 +93,26 @@ for (let i = 0; i < arr1.length; i++) {
             x = "не возрастает";
             i = (arr1.lenght - 1);
         }
-
     } 
 }
 // Проверка, возрастает ли массив
 
 for (let i = 0; i < arr1.length; i++) {
     if (i !== 0) {
-
-        if (arr1[i] <= arr1[i-1]) {
-           
-        } else {
+        if (arr1[i] > arr1[i-1]) {
             z = "не убывает";
             i = (arr1.lenght - 1);
         }
-
     } 
 }
 // Проверка, убывает ли массив
 
 for (let i = 0; i < arr1.length; i++) {
     if (i !== 0) {
-
-        if (arr1[i] == arr1[i-1]) {
-           
-        } else {
+        if (arr1[i] !== arr1[i-1]) {
             y = "значения разные";
             i = (arr1.lenght - 1);
         }
-
     } 
 }
 
@@ -139,11 +120,11 @@ if (x == "не возрастает" && z == "не убывает" && y == "зн
     console.log("Массив НЕ монотонный");
 } else {
     console.log("Массив монотонный");
-} */
+}*/
 
 // 6 Изучение конструкторов в игровом формате
 
-/* class Soldier {
+/*class Soldier {
     constructor(name, weapon, damage, armor) {
         this.name = name;
         this.health = 300;
@@ -153,8 +134,8 @@ if (x == "не возрастает" && z == "не убывает" && y == "зн
     }
 }
 
-let user1 = new Soldier("Санек", "СВД 1963", 1000, 0);
-let user2 = new Soldier("Rudolf", "Люгер 1901", 200, 800);
+let user1 = new Soldier("Санек", "СВД 1963", 1000, 0),
+    user2 = new Soldier("Rudolf", "Люгер 1901", 200, 800);
 
 let game = () => {
     let x = Math.floor((Math.random() * 10));
@@ -195,22 +176,23 @@ let game = () => {
         }, 3000);
     }
 };
-game(); */
+game();*/
 
 // 7 Создать массив arr = [] и записать в него 7 любых многозначных 
-// чисел в виде строк. Вывести в консоль только те, что начинаются с 
-// цифры 3 или 7 (Должны присутствовать в массиве) 
+// чисел. Вывести в консоль только те, что начинаются с цифры 3 или 7
+// (Должны присутствовать в массиве) 
 
-/* let arr = [243, 11, 35, 0, 3000, 776, 910]; 
+/*let arr = [243, 11, 35, 0, 3000, 776, 910],
+    arr2 = [];
 
-let sortFunc = (arr) => { 
-for (let i = 0; i < arr.length; i++) { 
-if (arr[i].toString()[0] == "3" || arr[i].toString()[0] == "7") { 
-console.log(arr[i]); 
-} 
-} 
-}; 
-sortFunc(arr); */
+let sortFunc = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].toString()[0] == "3" || arr[i].toString()[0] == "7") {
+            arr2.push(arr[i]);
+        }
+    }
+};
+sortFunc(arr);*/
 
 // 8 У вас есть str = “урок-3-был слишком легким”. Сделать так, чтобы строка 
 // начиналась с большой буквы. Замените все “-” на пробелы. Вывести в консоль 
@@ -228,68 +210,65 @@ console.log(str);*/
 // 9 У вас есть массив arr = [20, 33, 1, “Человек”, 2, 3]. Вывести в консоль 
 // квадратный корень из суммы кубов его элементов (Да, человека нужно исключить) 
 
-/*let arr = [20, 33, "1", "Человек", 2, 3]; 
+/*let arr = [20, 33, "1", "Человек", 2, 3];
 
-let account = (arr) => { 
-for (let i = 0; i < arr.length; i++) { 
+let account = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
 
-if (typeof(+arr[i]) == "number" && isNaN(arr[i]) !== true) { 
-arr[i] = arr[i]**3; 
-} else { 
-arr.splice(i, 1); 
-i--; 
-} 
-} 
-// console.log(arr); 
+        if (typeof (+arr[i]) == "number" && isNaN(arr[i]) !== true) {
+            arr[i] = arr[i] ** 3;
+        } else {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    let result = 0;
 
-let result = 0; 
-for (let i = 0; i < arr.length; i++) { 
-result = result + arr[i]; 
-} 
-console.log(Math.sqrt(result)); 
-}; 
+    for (let i = 0; i < arr.length; i++) {
+        result = result + arr[i];
+    }
+    console.log(Math.sqrt(result));
+};
 account(arr);*/
 
 // 10 Создайте функцию, которая принимает 1 аргумент (название произвольное) 
-// Если как аргумент передана не строка - функция оповещает об этом пользователя 
-// В полученной (как аргумент) строке функция должна убрать все пробелы в 
-// начале и в конце. Если строка более 50 знаков - то после 50го символа часть 
-// текста скрывается и вместо них появляются три точки (...) 
+// Если как аргумент передана не строка - функция оповещает об этом 
+// пользователя. В полученной (как аргумент) строке функция должна убрать все 
+// пробелы в начале и в конце. Если строка более 50 знаков - то после 50-го 
+// символа часть текста скрывается и вместо них появляются три точки (...) 
 
-/*let x = " 1234512345123451234512345123451234512345123451234512345 "; 
+/*let x = " 1234512345123451234512345123451234512345123451234512345 ";
 
-let clearFucn = (x) => { 
+let clearFucn = (x) => {
+    if (typeof (x) == "string") {
 
-if (typeof (x) == "string") { 
+        x = x.split("");
+        x.reverse();
 
-x = x.split(""); 
+        let revFunc = () => {
+            for (let i = 0; i < x.length; i++) {
+                if (x[i] == " ") {
+                    x.splice(i, 1);
+                    i--;
+                } else {
+                    i = x.length - 1;
+                }
+            }
+        };
 
-x.reverse(); 
+        revFunc(x);
+        x.reverse();
+        revFunc(x);
 
-let revFunc = () => { 
-for (let i = 0; i < x.length; i++) { 
-if (x[i] == " ") { 
-x.splice(i, 1); 
-i--; 
-} else { 
-i = x.length - 1; 
-} 
-} 
-}; 
+        if (x[50] !== undefined) {
 
-revFunc(x); 
-x.reverse(); 
-revFunc(x); 
-
-if (x[50] !== undefined) { 
-
-x = x.slice(0, 50); 
-x.push(".", ".", "."); 
-} 
-x = x.join(""); 
-console.log(x); 
-} else { 
-console.log("Аргумент передан не как строка"); 
-} 
-}; 
+            x = x.slice(0, 50);
+            x.push(".", ".", ".");
+        }
+        x = x.join("");
+        console.log(x);
+    } else {
+        console.log("Аргумент передан не как строка");
+    }
+};
 clearFucn(x);*/
