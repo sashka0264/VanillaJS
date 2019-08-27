ed3f6f838eaa1304a3d5b546426354a574b0bf10
Alexander <sashka0264@yandex.ru>
2019-08-27 14:34:11 +0300

    Внес задачу

--- task-3/script.js
+++ task-3/script.js
@@ -60,4 +60,34 @@ console.log(str);*/
 
 console.log(String(exp).substr(0, 2));*/
 
-// 5 
+// 5 Есть 4 бутылки с жидкостями, в одной из них смертельный яд, так же есть 2 
+// лабораторные мыши. Нужно определить в какой из бутылок яд за 24 часа. 
+// Известно, что яд действует в течение 18 часов (может подействовать через час,
+// а может и через 18 часов).
+
+/*let mouse1 = [],
+    mouse2 = [],
+    bottle = [1, 2, 3, 4],
+    toxic = Math.floor(Math.random() * 4) + 1;
+
+bottle.forEach((item, i) => {
+    if (item == toxic) {
+        bottle[i] = "toxic";
+    }
+});
+
+console.log(bottle);
+mouse1.push(bottle[0], bottle[3]);
+mouse2.push(bottle[2], bottle[3]);
+
+if ((mouse1[0] == "toxic" || mouse1[1] == "toxic") &&
+    (mouse2[0] == "toxic" || mouse2[1] == "toxic")) {
+    console.log("Обе умерли, яд в 4-й бутылке");
+} else if ((mouse1[0] !== "toxic" && mouse1[1] !== "toxic") &&
+    (mouse2[0] !== "toxic" && mouse2[1] !== "toxic")) {
+    console.log("Обе живы, яд во 2-й бутылке");
+} else if (mouse1[0] == "toxic" || mouse1[1] == "toxic") {
+    console.log("Яд в 1-й бутылке");
+} else if (mouse2[0] == "toxic" || mouse2[1] == "toxic") {
+    console.log("Яд в 3-й бутылке");
+}*/
